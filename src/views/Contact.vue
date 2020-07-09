83a4c4e7ae650098b0f2fd879e7fe8e7ba8f67b5
dingzhengru <qws7869vdx@gmail.com>
2020-07-09 11:28:52 +0800

    feat(contact): 完成 service 的功能

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -2,36 +2,50 @@
   <div class="lay-are-pageContent">
     <div class="theme-content-box are-contact">
       <ul class="cpn-boxzero cpn-inBlock-row ul-contact">
-        <li class="li-contact">
-          <a href="javascript:" class="lnk-contact lnk-contact-service" onclick="CSBind()">Online Customer Service</a>
+        <li class="li-contact" v-if="contact.service.isActive">
+          <a href="javascript:;" class="lnk-contact lnk-contact-service">
+            Online Customer Service
+          </a>
         </li>
-        <li class="li-contact">
+
+        <!-- <li class="li-contact">
+          <a href="javascript:;" class="lnk-contact lnk-contact-service" onclick="CSBind()">Online Customer Service</a>
+        </li> -->
+        <li class="li-contact" v-if="contact.facebook.isActive">
+          <a :href="contact.facebook.link" target="_blank" class="lnk-contact lnk-contact-facebook">Facebook</a>
+        </li>
+        <li class="li-contact" v-if="contact.mobile.isActive">
           <a
-            href="https://www.facebook.com/pg/lionking88.net/posts/?ref=page_internal"
-            target="_blank"
-            class="lnk-contact lnk-contact-facebook"
-            >Facebook</a
+            href="javascript:;"
+            class="lnk-contact lnk-contact-mobile"
+            @click="isShowMobileTelephones = !isShowMobileTelephones"
+            @blur="isShowMobileTelephones = false"
+            >Mobile</a
           >
+          <transition name="fade">
+            <div class="blk-tel" v-if="isShowMobileTelephones">
+              <a class="lnk-contact-tel" :href="`tel:${tel}`" v-for="tel in contact.mobile.telephones" :key="tel">
+                {{ tel }}
+              </a>
+            </div>
+          </transition>
         </li>
-        <li class="li-contact">
-          <a href="javascript:" class="lnk-contact lnk-contact-mobile">Mobile</a>
-          <div class="blk-tel">
-            <a href="tel:0854591000" class="lnk-contact-tel">085-459-1000</a><br />
-            <a href="tel:0854592000" class="lnk-contact-tel">085-459-2000</a><br />
-            <a href="tel:0854593000" class="lnk-contact-tel">085-459-3000</a><br />
-          </div>
+        <li class="li-contact" v-if="contact.skype.isActive">
+          <a :href="contact.skype.link" class="lnk-contact lnk-contact-skype">Skype</a>
         </li>
-        <li class="li-contact"><a href="skype:info.lionking88?call" class="lnk-contact lnk-contact-skype">Skype</a></li>
-        <li class="li-contact" style="display:none">
-          <a href="javascript:" class="lnk-contact lnk-contact-qq">QQ</a>
+        <!-- <li class="li-contact">
+          <a href="javascript:;" class="lnk-contact lnk-contact-qq">QQ</a>
           <div class="blk-qq">
             <a href="tencent://message/?uin=1448366105&amp;Menu=yes" class="lnk-contact-qq2">1448366105</a><br />
             <a href="tencent://message/?uin=1279982976&amp;Menu=yes" class="lnk-contact-qq2">1279982976</a><br />
             <a href="tencent://message/?uin=2272806809&amp;Menu=yes" class="lnk-contact-qq2">2272806809</a>
           </div>
+        </li> -->
+        <li class="li-contact" v-if="contact.line.isActive">
+          <a :href="contact.line.link" class="lnk-contact lnk-contact-line">{{ contact.line.name }}</a>
         </li>
-        <li class="li-contact">
-          <a href="http://line.me/ti/p/tIOnYUNLTa" class="lnk-contact lnk-contact-line">lionking_88</a>
+        <li class="li-contact" v-if="contact.wechat.isActive">
+          <a :href="contact.wechat.link" class="lnk-contact lnk-contact-wechat">{{ contact.wechat.name }}</a>
         </li>
       </ul>
     </div>
@@ -46,9 +60,40 @@ export default {
   computed: {
     ...mapGetters(['lang', 'templateType', 'templateVersion', 'templateVersionNumber']),
   },
+  data() {
+    return {
+      contact: {
+        service: {
+          isActive: true,
+        },
+        facebook: {
+          isActive: true,
+          link: 'https://www.facebook.com/pg/lionking88.net/posts/?ref=page_internal',
+        },
+        mobile: {
+          isActive: true,
+          telephones: ['085-459-1000', '085-459-2000', '085-459-3000'],
+        },
+        skype: {
+          isActive: true,
+          link: 'skype:info.lionking88?call',
+        },
+        line: {
+          isActive: true,
+          link: 'http://line.me/ti/p/tIOnYUNLTa',
+          name: 'lionking_88',
+        },
+        wechat: {
+          isActive: true,
+          link: 'http://weixin.qq.com/r/7Vbt4gzEXDIjhx2BnwPJ',
+          name: 'lionking_88',
+        },
+      },
+      isShowMobileTelephones: false,
+    };
+  },
   mounted() {
-
-    const templatePath = `${this.templateType}/${this.templateVersion}/${this.templateVersionNumber}`
+    const templatePath = `${this.templateType}/${this.templateVersion}/${this.templateVersionNumber}`;
     import(`@/styles/${this.templateType}/common/service.css`);
     import(`@/styles/${templatePath}/service.scss`);
   },
@@ -56,4 +101,15 @@ export default {
 </script>
 
 <style scoped>
+.lnk-contact-tel {
+  display: block;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
 </style>
