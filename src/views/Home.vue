53cd5d3ec46a63a250174abcb2a2fae2a759ccc6
dingzhengru <qws7869vdx@gmail.com>
2020-07-09 14:17:29 +0800

    feat(home): 把 index2.css 改成 home.css

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,14 +6,24 @@
 </template>
 
 <script>
-
-
+import { mapGetters } from 'vuex';
 
 export default {
   name: 'Home',
   components: {
     HomeSwiper: () => import('@/components/Y/home/HomeSwiper'),
-    HomeGameBlock: () => import('@/components/Y/home/HomeGameBlock')
+    HomeGameBlock: () => import('@/components/Y/home/HomeGameBlock'),
+  },
+  computed: {
+    ...mapGetters(['lang', 'templateType', 'templateVersion', 'templateVersionNumber']),
+  },
+  data() {
+    return {};
+  },
+  mounted() {
+    const templatePath = `${this.templateType}/${this.templateVersion}/${this.templateVersionNumber}`;
+    import(`@/styles/${this.templateType}/common/home.css`);
+    import(`@/styles/${templatePath}/home.scss`);
   },
 };
 </script>
