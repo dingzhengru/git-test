3a415aeaf3bfb449fab43c4effd487f283a7a9e2
dingzhengru <qws7869vdx@gmail.com>
2020-07-16 18:01:57 +0800

    feat(withdraw): 快完成此頁面，還沒處理密碼

--- src/settings.js
+++ src/settings.js
@@ -1,14 +1,23 @@
 const API_URL = 'http://localhost:3000/api';
 //const API_URL = 'http://api.caza7.re888show.com/api',
 
+// 預設語系 (以後應該會由後端取得)
 const DEFAULT_LANG = 'th-th';
 
 // 需加密的 API
 const CRYPTO_API_LIST = [];
 
-export { API_URL, DEFAULT_LANG, CRYPTO_API_LIST };
+// 需要登入才能進入的 route name(利用 beforeEach & store.getters.token)
+const AUTH_ROUTE_LIST = [];
+
+// 需要開通才能進入的 route name (利用 beforeEach & store.getters.isAccessed)
+const ACCESS_ROUTE_LIST = [];
+
+export { API_URL, DEFAULT_LANG, CRYPTO_API_LIST, AUTH_ROUTE_LIST, ACCESS_ROUTE_LIST };
 export default {
   API_URL,
   DEFAULT_LANG,
   CRYPTO_API_LIST,
+  AUTH_ROUTE_LIST,
+  ACCESS_ROUTE_LIST,
 };
--- src/views/transaction/TransactionWithdraw.vue
+++ src/views/transaction/TransactionWithdraw.vue
@@ -1,11 +1,157 @@
 <template>
-  <div></div>
+  <form class="withdraw" @submit.prevent="submitWithdraw">
+    <ul class="theme-content-box are-withdrawal">
+      <li class="theme-li-dataView" v-for="item in accountInfoList" :key="item.name">
+        <span class="theme-dataView-header">{{ item.title }}</span>
+        <p class="theme-dataView-data" v-if="item.content">{{ item.content }}</p>
+
+        <div v-if="item.name == 'balance'">
+          <button
+            type="button"
+            class="ui-btn01 ui-btn-long btn-transferBack"
+            id="GamePointBackToMain"
+            @click="allGamePointBackToMain"
+          >
+            All to my wallet
+          </button>
+        </div>
+
+        <div v-if="item.name == 'amount'">
+          <input
+            class="ui-ipt theme-ipt-dataview ipt-withdrawal"
+            id="Add_WithdrswalsPoint"
+            type="number"
+            name="Add_WithdrswalsPoint"
+            maxlength="12"
+            size="20"
+            step="0.01"
+            title="Please enter amount of withdrawal"
+            v-model="amount"
+          />
+        </div>
+
+        <div v-if="item.name == 'password'">
+          <input
+            class="ui-ipt theme-ipt-dataview ipt-withdrawal"
+            id="Add_Withdrawals_Password"
+            type="password"
+            name="Add_Withdrawals_Password"
+            size="20"
+            title="Please enter your passwords"
+          />
+          <div class="theme-errorMsg" v-if="errorPassword">
+            <span class="theme-txt-errorMsg">{{ errorPassword }}</span>
+          </div>
+        </div>
+      </li>
+    </ul>
+    <div class="hightlightMsg" style="color:#ffff00;font-size:2em">
+      For the withdrawal amount, please fill in the hundred, the minimum withdrawal amount 500 THB
+    </div>
+    <div class="are-control">
+      <a
+        class="ui-btn01 ui-btn-long btn-send"
+        id="Withdrawals"
+        href="javascript:"
+        onclick="beforeSend();btnWithdrawalsSaveClick()"
+        >Submit</a
+      >
+    </div>
+    <ol class="ui-ol-memberNotice">
+      <li>If you have any additional questions about your account details ,please contact our online service.</li>
+    </ol>
+  </form>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
 export default {
   name: 'TransactionWithdraw',
+  computed: {
+    ...mapGetters(['lang', 'token', 'templateType', 'templateVersion', 'templateVersionNumber']),
+  },
+  data() {
+    return {
+      errorPassword: '',
+      accountInfoList: [
+        {
+          name: 'account',
+          title: 'Account',
+          content: 'ding01',
+        },
+        {
+          name: 'currency',
+          title: 'Specific Currency',
+          content: 'Baht',
+        },
+        {
+          name: 'balance',
+          title: 'Wallet balance',
+          content: '0.00',
+        },
+        {
+          name: 'amount',
+          title: 'Withdrawal Amount',
+          content: '',
+        },
+        {
+          name: 'bankName',
+          title: 'Name of Bank',
+          content: 'SCB',
+        },
+        {
+          name: 'bankAccount',
+          title: 'Bank Account',
+          content: '1111111',
+        },
+        {
+          name: 'bankBranch',
+          title: 'Name of Branch',
+          content: '分行00000',
+        },
+        {
+          name: 'accountName',
+          title: 'Account Name of Bank',
+          content: 'first last',
+        },
+        {
+          name: 'password',
+          title: 'Password for Withdrawal',
+          content: '',
+        },
+      ],
+      amount: 0,
+      password: '',
+    };
+  },
+  mounted() {
+    // * 根據版型引入 css
+    const templatePath = `${this.templateType}/${this.templateVersion}/${this.templateVersionNumber}`;
+    import(`@/styles/${templatePath}/transaction/withdraw.scss`);
+  },
+  methods: {
+    allGamePointBackToMain() {
+      console.log('allGamePointBackToMain');
+    },
+    submitWithdraw() {
+      console.log('submitWithdraw');
+    },
+  },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+/* .reg-main > */
+.are-withdrawal {
+  margin: 40px 0;
+}
+.are-control {
+  margin: 40px 0;
+  text-align: center;
+}
+/* .reg-main > .are-withdrawal > */
+.btn-transferBack {
+  display: block;
+  margin: 0 auto;
+}
+</style>
