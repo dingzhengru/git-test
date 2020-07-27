1738d3349a5c88baeaac6df715c02c0574644cfe
dingzhengru <qws7869vdx@gmail.com>
2020-07-27 14:46:35 +0800

    style(game): 更改 css，還改掉跟這頁有關的 layout，大多都是 ui ，但其實都只出現在此頁而已

--- src/views/game/GameHome.vue
+++ src/views/game/GameHome.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="game">
-    <div class="game-jackpot theme-jackpot">
-      <span id="proValue" class="ui-txt-jackpot-count">218,119,944</span>
+    <div class="game__jackpot">
+      <span class="game__jackpot__text">218,119,944</span>
     </div>
     <router-view />
   </div>
@@ -19,9 +19,24 @@ export default {
   padding-bottom: 119px;
 }
 
-.game-jackpot {
+.game__jackpot {
   position: relative;
+  height: 71px;
+  margin-bottom: 28px;
+  background-repeat: no-repeat;
+  background-position: center;
 }
 
-
+.game__jackpot__text {
+  position: absolute;
+  left: 246px;
+  top: 3px;
+  font-size: 42px;
+  font-family: Arial Black, sans-serif;
+  font-weight: bold;
+  color: #fffefe;
+  background: linear-gradient(to bottom, #989898 0%, #ffffff 100%);
+  background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
 </style>
--- src/views/game/GameLobby.vue
+++ src/views/game/GameLobby.vue
@@ -1,31 +1,31 @@
 <template>
-  <div>
-    <div class="theme-supply">
-      <ul class="cpn-boxzero cpn-inBlock-row ui-ul-supply" id="lobbyproduct">
+  <div class="game-lobby">
+    <div class="game-lobby__supply theme-supply">
+      <ul class="game-lobby__supply__ul">
         <li
-          class="ui-li-supply"
-          :class="[gameType.class, { 'ui-li-supply-active': $route.params.type == gameType.type }]"
+          class="game-lobby__supply__ul__li"
+          :class="[gameType.class, { 'game-lobby__supply__ul__li--active': $route.params.type == gameType.type }]"
           v-for="gameType in gameTypeList"
           :key="gameType.name"
         >
           <router-link
-            class="ui-lnk-supply"
+            class="game-lobby__supply__ul__li__link"
             :to="{ name: 'GameLobby', params: { type: gameType.type }, query: { category: 'all' } }"
           >
             {{ gameType.name }}
           </router-link>
         </li>
       </ul>
-      <div>
-        <ul class="cpn-boxzero cpn-inBlock-row ui-ul-category">
+      <div class="game-lobby__category">
+        <ul class="game-lobby__category__ul">
           <li
-            class="ui-li-category"
+            class="game-lobby__category__ul__li"
             v-for="category in categoryList"
             :key="category.name"
-            :class="{ 'ui-li-category-active': $route.query.category == category.value }"
+            :class="{ 'game-lobby__category__ul__li--active': $route.query.category == category.value }"
           >
             <router-link
-              class="ui-lnk-category lnk-category"
+              class="game-lobby__category__ul__li__link"
               :to="{ name: 'GameLobby', params: { type: $route.params.type }, query: { category: category.value } }"
             >
               {{ category.name }}
@@ -34,24 +34,24 @@
         </ul>
       </div>
     </div>
-    <div class="cpn-inBlock-row theme-inquire">
-      <input class="ui-ipt-inquire" type="text" v-model="search.text" placeholder="Search" />
-      <button class="ui-lnk-inquire"></button>
-      <input type="submit" class="ui-ipt-favorites" title="My Favorites" />
-      <a href="javascript:;" class="cpn-inBlock ui-lnk-transferNow">Transfer Now</a>
+    <div class="game-lobby__inquire theme-inquire">
+      <input class="game-lobby__inquire__search" type="text" v-model="search.text" placeholder="Search" />
+      <button class="game-lobby__inquire__search-icon"></button>
+      <input class="game-lobby__inquire__favorites" type="submit" title="My Favorites" />
+      <a class="game-lobby__inquire__transfer-now" href="javascript:;">Transfer Now</a>
     </div>
     <div class="theme-list">
-      <table class="table-game">
+      <table class="game-lobby__table">
         <tbody>
-          <tr class="tr-game" v-for="(game, index) in pageData" :key="index">
-            <td class="td-1st">
-              <img class="img-gameBg" :src="game.img" />
+          <tr class="game-lobby__table__tr" v-for="(game, index) in pageData" :key="index">
+            <td class="game-lobby__table__tr__td-1st">
+              <img class="game-lobby__table__tr__td__img" :src="game.img" />
             </td>
-            <td class="td-2nd">{{ game.name }}</td>
-            <td class="td-3rd">
-              <a href="javascript:;" class="cpn-inBlock lnk-game-start">Play Now</a>
-              <a href="javascript:;" class="cpn-inBlock lnk-game-freeplay">Free Play</a>
-              <a href="javascript:;" class="cpn-inBlock lnk-game-favorites">Favorites</a>
+            <td class="game-lobby__table__tr__td-2nd">{{ game.name }}</td>
+            <td class="game-lobby__table__tr__td-3rd">
+              <a href="javascript:;" class="game-lobby__table__tr__td__link--start">Play Now</a>
+              <a href="javascript:;" class="game-lobby__table__tr__td__link--freeplay">Free Play</a>
+              <a href="javascript:;" class="game-lobby__table__tr__td__link--favorites">Favorites</a>
             </td>
           </tr>
         </tbody>
@@ -218,28 +218,131 @@ export default {
 </script>
 
 <style>
-.ui-lnk-transferNow {
-  font-size: 21px;
-  letter-spacing: 0;
-  padding: 22px 8px 0 48px;
-  line-height: 2;
+.game-lobby__supply__ul {
+  width: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  margin: 0;
+  padding: 11px 0;
+  white-space: nowrap;
+  overflow: auto;
 }
 
-.table-game {
+.game-lobby__supply__ul__li {
+  display: inline-block;
+  letter-spacing: normal;
+  word-spacing: normal;
+  background-position: center top;
+  background-repeat: no-repeat;
+  margin: 0 21px;
+}
+
+.game-lobby__supply__ul__li--active > .game-lobby__supply__ul__li__link {
+  color: #ffbebc;
+  text-shadow: 0px 0px 8px #ff1c59;
+}
+
+.game-lobby__supply__ul__li__link {
+  min-width: 74px;
+  display: block;
+  padding: 56px 0 0 0;
+  font-size: 22px;
+  color: #92a0c0;
+  text-align: center;
+}
+
+.game-lobby__category__ul {
+  width: 100%;
+  margin: 0;
+  padding: 0 15px;
+  white-space: nowrap;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  overflow: auto;
+}
+
+.game-lobby__category__ul__li {
+  font-size: 24px;
+  white-space: nowrap;
+  display: inline-block;
+}
+
+.game-lobby__category__ul__li:last-child {
+  padding-right: 20px;
+}
+
+.game-lobby__category__ul__li__link {
+  display: block;
+  padding: 0 15px;
+  line-height: 74px;
+}
+
+.game-lobby__inquire__search {
+  width: 302px;
+  background-size: cover;
+  -webkit-background-size: cover;
+  background-attachment: fixed;
+  height: 59px;
+  margin: 25px 0 31px 34px;
+  font-size: 24px;
+  border: 0;
+  text-indent: 20px;
+  outline: none;
+}
+
+.game-lobby__inquire__search-icon {
+  top: 19px;
+  left: 280px;
+  position: absolute;
+  width: 53px;
+  height: 71px;
+  border: 0;
+  text-indent: -9999px;
+  outline: none;
+}
+
+.game-lobby__inquire__favorites {
+  width: 85px;
+  height: 65px;
+  margin: 0 0 0 85px;
+  font-size: 24px;
+  text-indent: -9999px;
+  border: 0;
+  outline: none;
+}
+
+.game-lobby__inquire__transfer-now {
+  display: inline-block;
+  width: 133px;
+  height: 65px;
+  font-size: 21px;
+  letter-spacing: 0;
+  line-height: 2;
+  padding: 22px 8px 0 48px;
+  margin: 13px 0 0 10px;
+  color: #fedebe;
+  vertical-align: top;
+  background: url(~@/assets/common/imgs/ui/btn_transferNow.png) left center no-repeat;
+}
+
+.game-lobby__table {
   width: 100%;
   letter-spacing: -1px;
 }
-.table-game .td-1st {
+
+.game-lobby__table__tr__td-1st {
   max-width: 152px;
   padding: 0 10px;
   overflow: hidden;
 }
 
-.table-game .img-gameBg {
+.game-lobby__table__tr__td__img {
   width: 100%;
 }
 
-.table-game .td-2nd {
+.game-lobby__table__tr__td-2nd {
   width: auto;
   min-width: 208px;
   padding: 0 5px;
@@ -250,25 +353,20 @@ export default {
   word-break: break-word;
 }
 
-.table-game .td-3rd {
+.game-lobby__table__tr__td-3rd {
   width: auto;
   padding: 10px 0;
   white-space: nowrap;
   text-align: center;
 }
 
-.table-game .td-3rd a {
+.game-lobby__table__tr__td-3rd > a {
+  display: inline-block;
+  width: 90px;
+  height: 83px;
   color: #fff;
   font-size: 21px;
   line-height: 205px;
-}
-
-.lnk-game-start,
-.lnk-game-freeplay,
-.lnk-game-favorites,
-.lnk-game-favorites-selected {
-  width: 90px;
-  height: 83px;
   margin: 0 10px;
   background-repeat: no-repeat;
 }
