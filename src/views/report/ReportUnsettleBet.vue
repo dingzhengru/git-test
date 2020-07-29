65b2233b2916b952b701869a73ec68f30cf8c7c5
dingzhengru <qws7869vdx@gmail.com>
2020-07-29 11:26:45 +0800

    feat(report-unsettle-bet): 完成此頁

--- src/views/report/ReportUnsettleBet.vue
+++ src/views/report/ReportUnsettleBet.vue
@@ -1,13 +1,50 @@
 <template>
   <div class="report-unsettle-bet">
-    
+    <div class="report-unsettle-bet__box theme-content-box">
+      <table class="report-unsettle-bet__table ui-table02" v-for="item in list" :key="item.id">
+        <tbody>
+          <tr v-for="(value, key) in item" :key="key">
+            <th class="th-1st">{{ key }}</th>
+            <td class="td-2nd">{{ value }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
 <script>
+import numeral from 'numeral';
 export default {
   name: 'ReportUnsettleBet',
+  data() {
+    return {
+      numeral: numeral,
+      list: [
+        {
+          id: '000',
+          game: 'Sports-SBO Sports',
+          betCount: 0,
+          betAmount: 100,
+        },
+        {
+          id: '111',
+          game: 'Sports-SBO Sports',
+          betCount: 0,
+          betAmount: 100,
+        },
+      ],
+    };
+  },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.report-unsettle-bet {
+  margin-top: 30px;
+}
+.report-unsettle-bet__table {
+  margin-bottom: 10px;
+}
+
+</style>
