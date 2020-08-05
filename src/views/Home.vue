cfa4e612b23e89f8068dd365f0bad0ec6784057b
dingzhengru <qws7869vdx@gmail.com>
2020-08-05 11:30:03 +0800

    feat(home): 切換 lang 時會再發 request 去更新 swiper, product

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -16,7 +16,7 @@ export default {
     HomeGameBlock: () => import('@/components/home/HomeGameBlock'),
   },
   computed: {
-    ...mapGetters(['isLoggedIn', 'siteID', 'siteFullCss', 'resourceUrl', 'siteIsNewPromotion']),
+    ...mapGetters(['siteID', 'siteFullCss', 'lang', 'isLoggedIn', 'resourceUrl', 'siteIsNewPromotion']),
   },
   data() {
     return {
@@ -25,16 +25,31 @@ export default {
     };
   },
   mounted() {
-    // * 取得遊戲館列表，因不需要 siteID 所以放這即可
-    const requestDataProductList = {
-      DeviceType: 1,
-    };
-    getProductList(requestDataProductList).then(result => {
-      if (result.Code == 200) {
-        this.productList = result.RetObj;
-        console.log('[Product]', this.productList);
-      }
-    });
+    //* 取得遊戲館列表，因不需要 siteID 所以放這即可
+    this.getProductList();
+  },
+  methods: {
+    getProductList() {
+      const requestDataProductList = { DeviceType: 1 };
+      getProductList(requestDataProductList).then(result => {
+        if (result.Code == 200) {
+          this.productList = result.RetObj;
+          console.log('[Product]', this.productList);
+        }
+      });
+    },
+    getSwiperList() {
+      const requestDataSwiperList = {
+        DeviceType: 1,
+        bNewPromotion: this.siteIsNewPromotion,
+      };
+      getSwiperList(requestDataSwiperList).then(result => {
+        if (result.Code == 200) {
+          this.swiperList = result.RetObj;
+          console.log('[Swiper]', this.swiperList);
+        }
+      });
+    },
   },
   watch: {
     siteID: {
@@ -48,30 +63,15 @@ export default {
         import(`@/styles/${this.siteFullCss}/home.scss`);
 
         // * 取得輪播列表
-        const requestDataSwiperList = {
-          DeviceType: 1,
-          bNewPromotion: this.siteIsNewPromotion,
-        };
-        getSwiperList(requestDataSwiperList).then(result => {
-          if (result.Code == 200) {
-            this.swiperList = result.RetObj;
-            console.log('[Swiper]', this.swiperList);
-          }
-        });
+        this.getSwiperList();
       },
     },
     lang() {
       // * 取得輪播列表
-      const requestDataSwiperList = {
-        DeviceType: 1,
-        bNewPromotion: this.siteIsNewPromotion,
-      };
-      getSwiperList(requestDataSwiperList).then(result => {
-        if (result.Code == 200) {
-          this.swiperList = result.RetObj;
-          console.log('[Swiper]', this.swiperList);
-        }
-      });
+      this.getSwiperList();
+
+      //* 取得遊戲館列表
+      this.getProductList();
     },
   },
 };
