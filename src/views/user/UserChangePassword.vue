2b3c4f6a12ce466f39087dc10c459fc7ce0e5a3f
dingzhengru <qws7869vdx@gmail.com>
2020-09-18 10:39:18 +0800

    style(change-password): 修改 css 為 scss

--- src/views/user/UserChangePassword.vue
+++ src/views/user/UserChangePassword.vue
@@ -5,28 +5,28 @@
       id="user-change-passowrd-form"
       @submit.prevent="submitChangePassword"
     >
-      <div class="user-change-password__input-div theme-input-box">
+      <div class="user-change-password__form__field theme-input-box">
         <span class="theme-input-header">{{ $t('user.changePassword.passwordOld') }}</span>
         <input class="ui-ipt" type="password" required minlength="6" pattern="^[a-zA-Z0-9]*$" v-model="passwordOld" />
         <div class="theme-errorMsg" v-if="errorPasswordOld">
           <span class="theme-txt-errorMsg">{{ errorPasswordOld }}</span>
         </div>
       </div>
-      <div class="user-change-password__input-div theme-input-box">
+      <div class="user-change-password__form__field theme-input-box">
         <span class="theme-input-header">{{ $t('user.changePassword.passwordNew') }}</span>
         <input class="ui-ipt" type="password" required minlength="6" pattern="^[a-zA-Z0-9]*$" v-model="passwordNew" />
         <div class="theme-errorMsg" v-if="errorPasswordNew">
           <span class="theme-txt-errorMsg">{{ errorPasswordNew }}</span>
         </div>
       </div>
-      <div class="user-change-password__input-div theme-input-box">
+      <div class="user-change-password__form__field theme-input-box">
         <span class="theme-input-header">{{ $t('user.changePassword.passwordCheck') }}</span>
         <input class="ui-ipt" type="password" required minlength="6" pattern="^[a-zA-Z0-9]*$" v-model="passwordCheck" />
         <div class="theme-errorMsg" v-if="errorPasswordCheck">
           <span class="theme-txt-errorMsg">{{ errorPasswordCheck }}</span>
         </div>
       </div>
-      <div class="user-change-password__input-div theme-input-box">
+      <div class="user-change-password__form__field theme-input-box">
         <span class="theme-input-header">{{ $t('user.changePassword.passwordRemember') }}</span>
         <toggle-button
           v-model="isRememberPassword"
@@ -148,7 +148,7 @@ export default {
  * Switch 開關，width: 640px, height: 100px
 */
 
-.user-change-password__input-div .v-switch-core {
+.user-change-password__form__field .v-switch-core {
   border-radius: 0 !important;
 }
 
@@ -156,7 +156,7 @@ export default {
  * 按鈕
 */
 
-.user-change-password__input-div .vue-js-switch .v-switch-core .v-switch-button {
+.user-change-password .vue-js-switch .v-switch-core .v-switch-button {
   width: 320px !important;
   border-radius: 0 !important;
 }
@@ -165,7 +165,7 @@ export default {
  * 按鈕在右邊時
  * 位置: (開關寬度/2) + (開關寬度/2 - 按鈕寬度) => (640/2) + (640/2 - 320)
 */
-.user-change-password__input-div .vue-js-switch.toggled .v-switch-core .v-switch-button {
+.user-change-password .vue-js-switch.toggled .v-switch-core .v-switch-button {
   transform: translate3d(320px, 0px, 0px) !important;
 }
 /*
@@ -173,15 +173,15 @@ export default {
  * 位置: (按鈕大小 - 標籤大小) / 2 => (320-80) / 2
  * 但不同語言的標籤大小會不同
 */
-.zh-cn .user-change-password__input-div .vue-js-switch .v-switch-label.v-left {
+.zh-cn .user-change-password .vue-js-switch .v-switch-label.v-left {
   left: 120px !important;
 }
 
-.en-us .user-change-password__input-div .vue-js-switch .v-switch-label.v-left {
+.en-us .user-change-password .vue-js-switch .v-switch-label.v-left {
   left: 82px !important;
 }
 
-.th-th .user-change-password__input-div .vue-js-switch .v-switch-label.v-left {
+.th-th .user-change-password .vue-js-switch .v-switch-label.v-left {
   left: 100px !important;
 }
 
@@ -189,28 +189,43 @@ export default {
  * 右邊標籤(文字)
  * 位置: 同上
 */
-.zh-cn .user-change-password__input-div .vue-js-switch .v-switch-label.v-right {
+.zh-cn .user-change-password .vue-js-switch .v-switch-label.v-right {
   right: 120px !important;
 }
-.en-us .user-change-password__input-div .vue-js-switch .v-switch-label.v-right {
+.en-us .user-change-password .vue-js-switch .v-switch-label.v-right {
   right: 76px !important;
 }
-.th-th .user-change-password__input-div .vue-js-switch .v-switch-label.v-right {
+.th-th .user-change-password .vue-js-switch .v-switch-label.v-right {
   right: 95px !important;
 }
 </style>
 
+<style lang="scss" scoped>
+.user-change-password {
+  &__form {
+    margin: 40px 0;
+    &__field {
+      margin: 20px 0;
+    }
+  }
+  &__button-div {
+    margin: 40px 0;
+    text-align: center;
+  }
+}
+</style>
+
 <style scoped>
-.user-change-password__form {
+/* .user-change-password__form {
   margin: 40px 0;
-}
+} */
 
-.user-change-password__input-div {
+/* .user-change-password__form__field {
   margin: 20px 0;
-}
+} */
 
-.user-change-password__button-div {
+/* .user-change-password__button-div {
   margin: 40px 0;
   text-align: center;
-}
+} */
 </style>
