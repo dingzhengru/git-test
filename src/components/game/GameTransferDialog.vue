05f29b6c5f58e4c46ccaff58e992ca0fd11b64b5
dingzhengru <qws7869vdx@gmail.com>
2020-09-18 17:13:50 +0800

    style(GameTransferDialog): 修改 css 為 scss

--- src/components/game/GameTransferDialog.vue
+++ src/components/game/GameTransferDialog.vue
@@ -15,9 +15,9 @@
             <span>{{ currentPointProduct.Product_Name }}： {{ currentPointProduct.Point }}</span>
           </div>
 
-          <div class="game-transfer-dialog__form-switch-div">
+          <div class="game-transfer-dialog__form__switch-div">
             <button
-              class="game-transfer-dialog__form-switch left"
+              class="game-transfer-dialog__form__switch left"
               type="button"
               :class="{ on: isTransferAll }"
               @click.capture.stop="isTransferAll = true"
@@ -25,7 +25,7 @@
               {{ $t('game.transfer.transferAll') }}
             </button>
             <button
-              class="game-transfer-dialog__form-switch right"
+              class="game-transfer-dialog__form__switch right"
               type="button"
               :class="{ on: !isTransferAll }"
               @click.capture.stop="isTransferAll = false"
@@ -33,7 +33,7 @@
               {{ $t('game.transfer.transferByEnter') }}
             </button>
             <input
-              class="game-transfer-dialog__form-input"
+              class="game-transfer-dialog__form__input"
               type="number"
               v-model.number="amount"
               v-if="!isTransferAll"
@@ -108,7 +108,7 @@ export default {
 };
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .game-transfer-dialog-wrapper {
   position: fixed;
   top: 0;
@@ -119,6 +119,7 @@ export default {
   width: 100%;
   z-index: 9999;
 }
+
 .game-transfer-dialog {
   width: 80%;
   /* height: 628px; */
@@ -132,33 +133,113 @@ export default {
   z-index: 9999;
   font-size: 2.5rem;
   padding: 40px 30px 40px 30px;
+
+  &__title {
+    margin-bottom: 30px;
+    font-size: 3rem;
+    text-align: center;
+    font-weight: bold;
+  }
+  &__form {
+    margin-bottom: 60px;
+
+    &__text {
+      text-align: left;
+      margin-left: 30px;
+      color: #959595;
+
+      span {
+        color: #d6c388;
+      }
+    }
+    &__switch-div {
+      margin-top: 20px;
+    }
+    &__switch {
+      display: inline-block;
+      width: 261px;
+      height: 100px;
+      background-color: transparent;
+      background-repeat: no-repeat;
+      font-weight: bold;
+      padding-left: 50px;
+      /* padding: 25px 0px 30px 50px; */
+      color: #686868;
+      border: 0;
+      vertical-align: middle;
+    }
+    &__switch.on {
+      color: black;
+    }
+    &__input {
+      width: 90%;
+      padding: 16px 0 16px 20px;
+      font-size: 34px;
+      background-color: #2d2d2d;
+      border-radius: 6px;
+      border: 1px solid #7e7e7e;
+      margin-top: 20px;
+      box-sizing: border-box;
+    }
+    &__button {
+      margin-top: 30px;
+      width: 90%;
+      background-image: url(~@/assets/common/imgs/ui/btn--red.jpg);
+    }
+  }
 }
-.game-transfer-dialog__title {
+</style>
+
+<style scoped>
+/* .game-transfer-dialog-wrapper {
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+  z-index: 9999;
+} */
+/* .game-transfer-dialog {
+  width: 80%;
+  position: absolute;
+  top: 25%;
+  left: 0;
+  right: 0;
+  margin: auto;
+  text-align: center;
+  overflow: auto;
+  z-index: 9999;
+  font-size: 2.5rem;
+  padding: 40px 30px 40px 30px;
+} */
+/* .game-transfer-dialog__title {
   margin-bottom: 30px;
   font-size: 3rem;
   text-align: center;
   font-weight: bold;
-}
+} */
 
-.game-transfer-dialog__form {
+/* .game-transfer-dialog__form {
   margin-bottom: 60px;
-}
+} */
 
-.game-transfer-dialog__form__text {
+/* .game-transfer-dialog__form__text {
   text-align: left;
   margin-left: 30px;
   color: #959595;
-}
+} */
 
-.game-transfer-dialog__form__text span {
+/* .game-transfer-dialog__form__text span {
   color: #d6c388;
-}
+} */
 
-.game-transfer-dialog__form-switch-div {
+/* .game-transfer-dialog__form__switch-div {
   margin-top: 20px;
-}
+} */
 
-.game-transfer-dialog__form-switch {
+/* .game-transfer-dialog__form__switch {
   display: inline-block;
   width: 261px;
   height: 100px;
@@ -166,17 +247,16 @@ export default {
   background-repeat: no-repeat;
   font-weight: bold;
   padding-left: 50px;
-  /* padding: 25px 0px 30px 50px; */
   color: #686868;
   border: 0;
   vertical-align: middle;
-}
+} */
 
-.game-transfer-dialog__form-switch.on {
+/* .game-transfer-dialog__form__switch.on {
   color: black;
-}
+} */
 
-.game-transfer-dialog__form-input {
+/* .game-transfer-dialog__form__input {
   width: 90%;
   padding: 16px 0 16px 20px;
   font-size: 34px;
@@ -185,11 +265,11 @@ export default {
   border: 1px solid #7e7e7e;
   margin-top: 20px;
   box-sizing: border-box;
-}
+} */
 
-.game-transfer-dialog__form__button {
+/* .game-transfer-dialog__form__button {
   margin-top: 30px;
   width: 90%;
   background-image: url(~@/assets/common/imgs/ui/btn--red.jpg);
-}
+} */
 </style>
