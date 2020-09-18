9e32451d0ef0d9f18c7f06c359f84f778e90b89e
dingzhengru <qws7869vdx@gmail.com>
2020-09-18 16:16:03 +0800

    style(AppFooter): 修改 css 為 scss

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -115,7 +115,7 @@ export default {
 };
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .footer {
   width: 100%;
   padding-top: 3px;
@@ -123,19 +123,104 @@ export default {
   left: 0;
   bottom: 0;
   z-index: 4;
+
+  &__ul {
+    padding: 0;
+    margin: 0;
+
+    &__li {
+      width: 25%;
+      display: inline-block;
+      vertical-align: top;
+
+      &__link {
+        display: table-cell;
+        width: 25%;
+        height: 119px;
+        color: #fff;
+        word-wrap: break-word;
+        word-break: keep-all;
+        text-align: center;
+        vertical-align: middle;
+        box-sizing: border-box;
+      }
+
+      &--regist > .footer__ul__li__link {
+        background-position: 24% 6px;
+        text-indent: 12px;
+      }
+      &--event > .footer__ul__li__link {
+        background-position: 50% 6px;
+      }
+      &--service > .footer__ul__li__link {
+        background-position: 75% 6px;
+      }
+      &--transaction > .footer__ul__li__link {
+        background-position: 97% 6px;
+      }
+    }
+    &__li:after {
+      display: inline-block;
+      content: '';
+      width: 4px;
+      height: 119px;
+      position: absolute;
+      top: 0;
+    }
+    &__li:first-child:after {
+      background: none;
+    }
+  }
 }
 
-.footer__ul {
+//* 語系
+
+.en-us {
+  .footer__ul__li__link {
+    padding: 30% 0 0 0;
+    font-size: 26px;
+    line-height: 27px;
+  }
+}
+
+.th-th {
+  .footer__ul__li__link {
+    padding: 24% 0 0 0;
+    font-size: 30px;
+    line-height: 28px;
+  }
+}
+
+.zh-cn {
+  .footer__ul__li__link {
+    padding: 27% 0 0 0;
+    font-size: 32px;
+    line-height: 38px;
+  }
+}
+</style>
+
+<style scoped>
+/* .footer {
+  width: 100%;
+  padding-top: 3px;
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  z-index: 4;
+} */
+
+/* .footer__ul {
   padding: 0;
   margin: 0;
-}
+} */
 
-.footer__ul__li {
+/* .footer__ul__li {
   width: 25%;
   display: inline-block;
   vertical-align: top;
-}
-.footer__ul__li__link {
+} */
+/* .footer__ul__li__link {
   display: table-cell;
   width: 25%;
   height: 119px;
@@ -145,51 +230,29 @@ export default {
   text-align: center;
   vertical-align: middle;
   box-sizing: border-box;
-}
-.footer__ul__li:after {
+} */
+/* .footer__ul__li:after {
   display: inline-block;
   content: '';
   width: 4px;
   height: 119px;
   position: absolute;
   top: 0;
-}
-.footer__ul__li:first-child:after {
+} */
+/* .footer__ul__li:first-child:after {
   background: none;
-}
-.footer__ul__li--regist > .footer__ul__li__link {
+} */
+/* .footer__ul__li--regist > .footer__ul__li__link {
   background-position: 24% 6px;
   text-indent: 12px;
-}
-.footer__ul__li--event > .footer__ul__li__link {
+} */
+/* .footer__ul__li--event > .footer__ul__li__link {
   background-position: 50% 6px;
-}
-.footer__ul__li--service > .footer__ul__li__link {
+} */
+/* .footer__ul__li--service > .footer__ul__li__link {
   background-position: 75% 6px;
-}
-.footer__ul__li--transaction > .footer__ul__li__link {
+} */
+/* .footer__ul__li--transaction > .footer__ul__li__link {
   background-position: 97% 6px;
-}
-
-/*
- * 語系
-*/
-
-.en-us .footer__ul__li__link {
-  padding: 30% 0 0 0;
-  font-size: 26px;
-  line-height: 27px;
-}
-
-.th-th .footer__ul__li__link {
-  padding: 24% 0 0 0;
-  font-size: 30px;
-  line-height: 28px;
-}
-
-.zh-cn .footer__ul__li__link {
-  padding: 27% 0 0 0;
-  font-size: 32px;
-  line-height: 38px;
-}
+} */
 </style>
