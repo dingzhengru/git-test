175fb5873be948267027d817837aaab426e873ff
dingzhengru <qws7869vdx@gmail.com>
2020-09-18 16:22:40 +0800

    style(DepositDialog): 修改 css 為 scss

--- src/components/transaction/deposit/DepositDialog.vue
+++ src/components/transaction/deposit/DepositDialog.vue
@@ -7,7 +7,10 @@
         <div class="deposit-dialog__title" v-html="$t('transaction.deposit.dialog.title')"></div>
         <ul class="deposit-dialog__ul">
           <li class="deposit-dialog__li" v-html="$t('transaction.deposit.dialog.notice.currency')"></li>
-          <li class="deposit-dialog__li" v-html="$t('transaction.deposit.dialog.notice.atm', { siteName: siteName })"></li>
+          <li
+            class="deposit-dialog__li"
+            v-html="$t('transaction.deposit.dialog.notice.atm', { siteName: siteName })"
+          ></li>
           <li class="deposit-dialog__li" v-html="$t('transaction.deposit.dialog.notice.violate')"></li>
         </ul>
         <div class="deposit-dialog__footer">
@@ -36,7 +39,7 @@ export default {
 };
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .deposit-dialog-wrapper {
   position: fixed;
   top: 0;
@@ -46,10 +49,62 @@ export default {
   height: 100%;
   width: 100%;
   z-index: 9999;
+
+  .deposit-dialog {
+    width: 70%;
+    position: absolute;
+    top: 25%;
+    left: 0;
+    right: 0;
+    margin: auto;
+    text-align: center;
+    overflow: auto;
+    z-index: 9999;
+    font-size: 2rem;
+    padding: 40px 30px 20px 30px;
+
+    &__title {
+      margin-bottom: 30px;
+      font-size: 2.5rem;
+      text-align: center;
+      font-weight: bold;
+    }
+    &__ul {
+      text-align: left;
+      list-style: decimal;
+      color: #f1cb80;
+      margin: 0;
+    }
+    &__footer {
+      margin-top: 20px;
+      color: #f1cb80;
+
+      &__phone {
+        font-size: 4rem;
+        margin-top: 20px;
+
+        & > a {
+          color: #fffeb3;
+        }
+      }
+    }
+  }
 }
-.deposit-dialog {
+</style>
+
+<style scoped>
+/* .deposit-dialog-wrapper {
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+  z-index: 9999;
+} */
+/* .deposit-dialog {
   width: 70%;
-  /* height: 628px; */
   position: absolute;
   top: 25%;
   left: 0;
@@ -60,29 +115,29 @@ export default {
   z-index: 9999;
   font-size: 2rem;
   padding: 40px 30px 20px 30px;
-}
-.deposit-dialog__title {
+} */
+/* .deposit-dialog__title {
   margin-bottom: 30px;
   font-size: 2.5rem;
   text-align: center;
   font-weight: bold;
-}
-.deposit-dialog__ul {
+} */
+/* .deposit-dialog__ul {
   text-align: left;
   list-style: decimal;
   color: #f1cb80;
   margin: 0;
-}
+} */
 /* .deposit-dialog__li {} */
-.deposit-dialog__footer {
+/* .deposit-dialog__footer {
   margin-top: 20px;
   color: #f1cb80;
-}
-.deposit-dialog__footer__phone {
+} */
+/* .deposit-dialog__footer__phone {
   font-size: 4rem;
   margin-top: 20px;
-}
-.deposit-dialog__footer__phone > a {
+} */
+/* .deposit-dialog__footer__phone > a {
   color: #fffeb3;
-}
+} */
 </style>
