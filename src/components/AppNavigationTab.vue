d065a2355602de966c7d35ead2f3ab60f1b8e126
dingzhengru <qws7869vdx@gmail.com>
2020-09-18 16:05:56 +0800

    style(AppNavigationTab): 修改 css 為 scss

--- src/components/AppNavigationTab.vue
+++ src/components/AppNavigationTab.vue
@@ -44,41 +44,72 @@ export default {
 };
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .tabs__ul {
   margin: 0;
   padding: 0;
   text-align: center;
-
   /* 避免換行 */
   /* white-space: nowrap; */
   /* word-break: break-word; */
+
+  &__li {
+    display: inline-block;
+    background-repeat: no-repeat;
+    background-position: center top;
+    border-right: 1px dotted #fff;
+    list-style: none;
+    vertical-align: top;
+    /* word-break: break-word; */
+    /* letter-spacing: normal; */
+    /* word-spacing: normal; */
+
+    &__link {
+      display: block;
+      min-width: 116px;
+      max-width: 140px;
+      margin: 0;
+      padding: 102px 20px 0px 20px;
+      /* margin: 0 25px; */
+      /* padding-top: 102px; */
+      color: #fff;
+      font-size: 2.153em;
+      text-align: center;
+    }
+  }
+  &__li:last-child {
+    border-right: none;
+  }
 }
-.tabs__ul__li {
+</style>
+
+<style scoped>
+/* .tabs__ul {
+  margin: 0;
+  padding: 0;
+  text-align: center;
+
+} */
+/* .tabs__ul__li {
   display: inline-block;
   background-repeat: no-repeat;
   background-position: center top;
   border-right: 1px dotted #fff;
   list-style: none;
   vertical-align: top;
-  /* word-break: break-word; */
-  /* letter-spacing: normal; */
-  /* word-spacing: normal; */
-}
-.tabs__ul__li:last-child {
+} */
+/* .tabs__ul__li:last-child {
   border-right: none;
-}
+} */
 
-.tabs__ul__li__link {
+/* .tabs__ul__li__link {
   display: block;
   min-width: 116px;
   max-width: 140px;
   margin: 0;
   padding: 102px 20px 0px 20px;
-  /* margin: 0 25px; */
-  /* padding-top: 102px; */
   color: #fff;
   font-size: 2.153em;
   text-align: center;
-}
+} */
 </style>
