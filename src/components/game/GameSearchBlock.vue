d9b2c8fbd69b01338c52abd974614e8885ed696d
dingzhengru <qws7869vdx@gmail.com>
2020-09-18 17:04:39 +0800

    style(GameSearchBlock): 修改 css 為 scss

--- src/components/game/GameSearchBlock.vue
+++ src/components/game/GameSearchBlock.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="game-lobby__inquire">
-    <form class="game-lobby__inquire-form" @submit.prevent="submitSearchForm">
+    <form class="game-lobby__inquire__form" @submit.prevent="submitSearchForm">
       <input
-        class="game-lobby__inquire__search"
+        class="game-lobby__inquire__form__search"
         type="search"
         v-model="cloneSearch.text"
         :placeholder="$t('game.placeholder.search')"
         @input="inputSearchText"
       />
-      <button class="game-lobby__inquire__search-icon" type="submit"></button>
+      <button class="game-lobby__inquire__form__search-icon" type="submit"></button>
     </form>
     <button class="game-lobby__inquire__favorites" @click="changeSearchIsLike" v-if="$route.params.type == 2"></button>
     <button class="game-lobby__inquire__button--transfer-now" @click="openTransferDialog">
@@ -50,17 +50,96 @@ export default {
 };
 </script>
 
-<style scoped>
-.game-lobby__inquire {
-  position: relative;
+<style lang="scss" scoped>
+.game-lobby {
+  &__inquire {
+    position: relative;
+
+    &__form {
+      display: inline-block;
+      position: relative;
+
+      &__search {
+        width: 302px;
+        background-size: cover;
+        -webkit-background-size: cover;
+        background-attachment: fixed;
+        height: 59px;
+        margin: 25px 0 31px 34px;
+        font-size: 24px;
+        border: 0;
+        text-indent: 20px;
+        outline: none;
+      }
+      &__search-icon {
+        top: 19px;
+        right: 0;
+        position: absolute;
+        width: 53px;
+        height: 71px;
+        border: 0;
+      }
+    }
+
+    &__favorites {
+      width: 85px;
+      height: 65px;
+      margin: 0 0 16px 85px;
+      font-size: 24px;
+      border: 0;
+      vertical-align: middle;
+    }
+
+    &__button--transfer-now {
+      display: inline-block;
+      width: 187px;
+      height: 65px;
+      /* font-size: 21px; */
+      padding: 10px 10px 10px 48px;
+      margin: 22px 10px 0 0;
+      color: #fedebe;
+      vertical-align: top;
+      background: url(~@/assets/common/imgs/ui/btn_transferNow.png) left center no-repeat;
+      border: none;
+      float: right;
+    }
+  }
 }
 
-.game-lobby__inquire-form {
+/*
+ * 語系
+*/
+
+.en-us {
+  .game-lobby__inquire__button--transfer-now {
+    font-size: 1.5rem;
+  }
+}
+
+.th-th {
+  .game-lobby__inquire__button--transfer-now {
+    font-size: 1.5rem;
+  }
+}
+
+.zh-cn {
+  .game-lobby__inquire__button--transfer-now {
+    font-size: 2.4rem;
+  }
+}
+</style>
+
+<style scoped>
+/* .game-lobby__inquire {
+  position: relative;
+} */
+
+/* .game-lobby__inquire__form {
   display: inline-block;
   position: relative;
-}
+} */
 
-.game-lobby__inquire__search {
+/* .game-lobby__inquire__form__search {
   width: 302px;
   background-size: cover;
   -webkit-background-size: cover;
@@ -71,34 +150,30 @@ export default {
   border: 0;
   text-indent: 20px;
   outline: none;
-}
+} */
 
-.game-lobby__inquire__search-icon {
+/* .game-lobby__inquire__form__search-icon {
   top: 19px;
-  /* left: 280px; */
-  /* top: 25px; */
   right: 0;
   position: absolute;
   width: 53px;
-  /* height: 59px; */
   height: 71px;
   border: 0;
-}
+} */
 
-.game-lobby__inquire__favorites {
+/* .game-lobby__inquire__favorites {
   width: 85px;
   height: 65px;
   margin: 0 0 16px 85px;
   font-size: 24px;
   border: 0;
   vertical-align: middle;
-}
+} */
 
-.game-lobby__inquire__button--transfer-now {
+/* .game-lobby__inquire__button--transfer-now {
   display: inline-block;
   width: 187px;
   height: 65px;
-  /* font-size: 21px; */
   padding: 10px 10px 10px 48px;
   margin: 22px 10px 0 0;
   color: #fedebe;
@@ -106,21 +181,5 @@ export default {
   background: url(~@/assets/common/imgs/ui/btn_transferNow.png) left center no-repeat;
   border: none;
   float: right;
-}
-
-/*
- * 語系
-*/
-
-.en-us .game-lobby__inquire__button--transfer-now {
-  font-size: 1.5rem;
-}
-
-.th-th .game-lobby__inquire__button--transfer-now {
-  font-size: 1.5rem;
-}
-
-.zh-cn .game-lobby__inquire__button--transfer-now {
-  font-size: 2.4rem;
-}
+} */
 </style>
