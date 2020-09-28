6692ae3f092cf6cfdd40d0814117ca9e96c4d319
ding <ding_ding@myplay20.com>
2020-09-28 13:25:35 +0800

    feat(promotion-detail): 請求資料的 ID 改為數字格式

--- src/views/promotion/PromotionDetail.vue
+++ src/views/promotion/PromotionDetail.vue
@@ -4,13 +4,20 @@
       <img class="promotion-detail__image__img" :src="image" alt="" />
     </div>
     <div class="promotion-detail__main">
-      <div class="promotion-detail__main__content" v-for="content in contentList" :key="content.name">
+      <div
+        class="promotion-detail__main__content"
+        v-for="content in contentList"
+        :key="content.name"
+      >
         <h3 class="promotion-detail__main__content__title">{{ $t(content.title) }}</h3>
         <div class="promotion-detail__main__content__desc" v-html="content.content"></div>
       </div>
       <div class="promotion-detail__main__button">
-        <button class="promotion-detail__main__button--back ui-btn ui-btn-long" @click="$router.go(-1)">
-          {{ $t('promotionDetail.button.back') }}
+        <button
+          class="promotion-detail__main__button--back ui-btn ui-btn-long"
+          @click="$router.go(-1)"
+        >
+          {{ $t("promotionDetail.button.back") }}
         </button>
       </div>
     </div>
@@ -18,32 +25,33 @@
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
-import { getPromotionDetail } from '@/api/promotion';
+import { mapGetters } from "vuex";
+import { getPromotionDetail } from "@/api/promotion";
 
-import event01 from '@/assets/Y/01/imgs/promotion/event01.jpg';
+import event01 from "@/assets/Y/01/imgs/promotion/event01.jpg";
 export default {
-  name: 'PromotionDetail',
+  name: "PromotionDetail",
   computed: {
-    ...mapGetters(['isLoggedIn', 'siteID', 'siteFullCss']),
+    ...mapGetters(["isLoggedIn", "siteID", "siteFullCss"]),
   },
   data() {
     return {
       image: event01,
       contentList: [
         {
-          name: 'date',
-          title: 'promotionDetail.title.date',
-          content: 'Starts 07/08/2017 at 11:00 (During the time member second deposit maximum can get 99,999THB.)',
+          name: "date",
+          title: "promotionDetail.title.date",
+          content:
+            "Starts 07/08/2017 at 11:00 (During the time member second deposit maximum can get 99,999THB.)",
         },
         {
-          name: 'content',
-          title: 'promotionDetail.title.content',
+          name: "content",
+          title: "promotionDetail.title.content",
           content: `<div class="promotion-detail__main__content__desc"><ul class="cpn-ul-decimal"><li>Minimum deposit 500 THB can join this promotion</li><li>Bonus up to 5% the amount of deposit . highest to 99,999THB and rolling request multiple by 5 times to cash out.</li><li>Exp.Deposit 500 bath get bonus 10% (1,000x5%=50) The member can withdraw if summary of wagering more than (1,000+50)x5 = 5,250</li></ul></div>`,
         },
         {
-          name: 'rule',
-          title: 'promotionDetail.title.rule',
+          name: "rule",
+          title: "promotionDetail.title.rule",
           content: `<ul class="cpn-ul-decimal"><li>Maximum bonus up to99,999</li><li>The same deposit amount. Can only get bonus one time.</li><li>Roling 5 times to cash out (Deposit+Bonus) and rolling will be counted when the amount of money taken bets will be win or lose</li><li>Member account multiple applications behavior will be reserves the right to cancel. If multiple players in the common use of an account or a single player use multiple accounts or any fraudulent activity, all wagers will be canceled</li><li>Live Casino: Baccaract bet “Banker+Player”, Roulette : Bet on “Red+ Black”，”Even+Odds“，”Big+Small“ are all does not count.</li><li>Live Casino Roulette , one game wagers over 25th will not count. Below 24(or including 24 ) will be normal betting.</li><li>Sport (SABA):Same match can not be bets opposite side.</li><li>We also reserve the right to change rules or confidentiality regulations at any time. Any change will be published and displayed at the website of『LIONKING88』</li></ul>`,
         },
       ],
@@ -51,11 +59,11 @@ export default {
   },
   methods: {
     async getPromotionDetail() {
-      console.log('[GetPromotionDetail]', this.$route.params.id);
+      console.log("[GetPromotionDetail]", this.$route.params.id);
 
-      const requestData = { PromotionId: this.$route.params.id };
+      const requestData = { PromotionId: Number(this.$route.params.id) };
       const result = await getPromotionDetail(requestData);
-      console.log('[PromotionDetail]', result);
+      console.log("[PromotionDetail]", result);
       // if (result.Code == 200) {
       //   console.log('PromotionDetail', result);
       // }
