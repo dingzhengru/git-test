6bc2778038cffedc5221ae1212bab1b04fec776f
dingzhengru <qws7869vdx@gmail.com>
2020-10-15 15:36:40 +0800

    feat(sw.js): cors fetch 的部分改成 workbox 的設定方式

--- public/sw.js
+++ public/sw.js
@@ -25,48 +25,70 @@ workbox.routing.registerRoute(
 );
 
 workbox.routing.registerRoute(
-  /(empty)/,
+  /^(http|https):\/\/resource.re888show.com.*$/,
   new workbox.strategies.StaleWhileRevalidate({
-    cacheName: 'global-static-file-cache',
+    cacheName: 'cors-re888show-static-file-catch',
+    fetchOptions: {
+      mode: 'cors',
+      credentials: 'omit',
+    },
   })
 );
 
+workbox.routing.registerRoute(
+  /^(http|https):\/\/resource.thsitea.com.*$/,
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: 'cors-thsitea-static-file-catch',
+    fetchOptions: {
+      mode: 'cors',
+      credentials: 'omit',
+    },
+  })
+);
+
+// workbox.routing.registerRoute(
+//   /(empty)/,
+//   new workbox.strategies.StaleWhileRevalidate({
+//     cacheName: 'global-static-file-cache',
+//   })
+// );
+
 // Fetch CORS assest
-addEventListener('fetch', function(e) {
-  // Put CORS resorce host here
-  const hosts = ['https://resource.thsitea.com'];
-  if (hosts.some(host => e.request.url.startsWith(host))) {
-    e.respondWith(
-      (async function() {
-        const cachedResponse = await caches.match(e.request);
-        if (cachedResponse) {
-          return cachedResponse;
-        }
+// addEventListener('fetch', function(e) {
+//   // Put CORS resorce host here
+//   const hosts = ['https://resource.thsitea.com'];
+//   if (hosts.some(host => e.request.url.startsWith(host))) {
+//     e.respondWith(
+//       (async function() {
+//         const cachedResponse = await caches.match(e.request);
+//         if (cachedResponse) {
+//           return cachedResponse;
+//         }
 
-        const networkResponse = await fetch(e.request, {
-          mode: 'cors',
-          credentials: 'omit',
-        });
-        if (networkResponse.status == '200') {
-          // if fetch success
-          const clonedResponse = networkResponse.clone();
+//         const networkResponse = await fetch(e.request, {
+//           mode: 'cors',
+//           credentials: 'omit',
+//         });
+//         if (networkResponse.status == '200') {
+//           // if fetch success
+//           const clonedResponse = networkResponse.clone();
 
-          e.waitUntil(
-            (async function() {
-              const cache = await caches.open('global-static-file-cache');
-              // This will be called after `return networkResponse`
-              await cache.put(e.request, clonedResponse);
-            })()
-          );
-        } else {
-          return;
-        }
+//           e.waitUntil(
+//             (async function() {
+//               const cache = await caches.open('global-static-file-cache');
+//               // This will be called after `return networkResponse`
+//               await cache.put(e.request, clonedResponse);
+//             })()
+//           );
+//         } else {
+//           return;
+//         }
 
-        return networkResponse;
-      })()
-    );
-  }
-});
+//         return networkResponse;
+//       })()
+//     );
+//   }
+// });
 
 self.addEventListener('message', event => {
   if (event.data && event.data.type === 'SKIP_WAITING') {
