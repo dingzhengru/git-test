0e98205461195ac2b75f9cbb26512b28db047704
dingzhengru <qws7869vdx@gmail.com>
2020-11-19 14:24:15 +0800

    fix(inboxDetail): 修正回覆信件的參數

--- src/views/notification/NotificationInboxDetail.vue
+++ src/views/notification/NotificationInboxDetail.vue
@@ -23,7 +23,7 @@
       >
         <ValidationProvider class="theme-input-box" tag="div" :rules="{ required: true }">
           <span class="theme-input-header">{{ $t('notification.inboxDetail.reply') }}</span>
-          <textarea class="ui-tar" cols="80" rows="5" v-model="content"></textarea>
+          <textarea class="ui-tar" cols="80" rows="5" v-model="mail.Add_Content"></textarea>
         </ValidationProvider>
       </form>
 
@@ -61,7 +61,12 @@ export default {
   data() {
     return {
       list: [],
-      content: '',
+      mail: {
+        Add_Category: '',
+        Add_Subject: '',
+        Add_ReplyPath: '',
+        Add_Content: '',
+      },
     };
   },
   methods: {
@@ -70,23 +75,20 @@ export default {
       const result = await apiGetInboxDetail(requestData);
 
       this.list = result.RetObj.Rows;
+      this.mail.Add_Category = result.RetObj.Add_Category;
+      this.mail.Add_Subject = result.RetObj.Add_Subject;
+      this.mail.Add_ReplyPath = result.RetObj.Add_ReplyPath;
     },
     async submitMail() {
-      const requestData = {
-        Add_Category: this.list[0].Lst_Category,
-        Add_Subject: this.list[0].Lst_Subject,
-        Add_Content: this.content,
-        Add_ReplyPath: this.list.map(item => item.Lst_Key).join(':'),
-      };
-      const result = await apiSendMail(requestData);
+      const result = await apiSendMail(this.mail);
 
       if (result.Code == 200) {
         window.alert(this.$t('alert.replaySuccess'));
-        this.content = '';
+        this.resetForm();
       }
     },
     resetForm() {
-      this.content = '';
+      this.mail.Add_Content = '';
     },
   },
   mounted() {
