755701c93aa6fc614f40ed96b284faba757a7bb2
dingzhengru <qws7869vdx@gmail.com>
2021-01-28 10:56:54 +0800

    feat(nav, pagination): 改進 css

---
+++ src/components/Z/transaction/TransactionHome.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="transaction">
+    <component :is="AppNavTab" :list="navList" />
+    <!-- <router-view /> -->
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+export default {
+  name: 'TransactionHome',
+  computed: {
+    ...mapGetters(['siteSetting', 'siteFullCss']),
+    AppNavTab() {
+      return () => import(`@/${this.siteSetting.components.user.AppNavTab}`);
+    },
+  },
+  data() {
+    return {
+      navList: [
+        {
+          name: 'deposit',
+          link: 'TransactionDeposit',
+          text: 'transaction.nav.deposit',
+          class: '',
+          id: 'transaction.nav.deposit',
+          otherActivePath: [],
+        },
+        {
+          name: 'withdrawal',
+          link: 'TransactionWithdrawal',
+          text: 'transaction.nav.withdrawal',
+          class: 'ui-li-tabs-withdrawal',
+          id: 'transaction.nav.withdrawal',
+          otherActivePath: [],
+        },
+        {
+          name: 'transfer',
+          link: 'TransactionTransfer',
+          text: 'transaction.nav.transfer',
+          class: '',
+          id: 'transaction.nav.transfer',
+          otherActivePath: [],
+        },
+        {
+          name: 'record',
+          link: 'TransactionRecordHome',
+          text: 'transaction.nav.record',
+          class: '',
+          id: 'transaction.nav.record',
+          otherActivePath: ['TransactionRecordContent', 'TransactionRecordDetail'],
+        },
+      ],
+    };
+  },
+  mounted() {
+    import(`@/styles/${this.siteFullCss}/transaction.scss`);
+  },
+};
+</script>
