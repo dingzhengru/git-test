a1a9d17381e490992b297d124c8fd8e0cb9c8a0f
dingzhengru <qws7869vdx@gmail.com>
2021-03-11 17:18:04 +0800

    feat(api): 測試取得 ip，改用 jsonp

--- src/api/ip.js
+++ src/api/ip.js
@@ -1,11 +1,19 @@
+// import jsonp from 'jsonp';
 import axios from 'axios';
 
 export async function getIP() {
-  const result = await axios.get('https://api.ipify.org/?format=json');
+  // jsonp('https://api.ipify.org/?format=jsonp', null, (err, data) => {
+  //   if (err) {
+  //     console.error(err.message);
+  //   } else {
+  //     console.log('data', data);
+  //   }
+  // });
+  const result = await axios.get('https://api.ipify.org/?format=jsonp');
   return result.data;
 }
 
-export async function getIPDetail(params) {
-  const result = await axios.get('https://wq.apnic.net/query', { params });
-  return result.data;
-}
+// export async function getIPDetail(params) {
+//   const result = await axios.get('https://wq.apnic.net/query', { params });
+//   return result.data;
+// }
