7139e1677a53c2c6661a326b7060cdb6eb4d1280
dingzhengru <qws7869vdx@gmail.com>
2021-04-01 17:12:12 +0800

    feat(test): 測試合併

--- src/settings-no-merge.js
+++ src/settings-no-merge.js
@@ -1,4 +1,5 @@
-//! 需於合併前，故意製造衝突，修改註解即可 
+//! 需於合併前，故意製造衝突，修改註解即可
+//! 或是利用 git reset <目標檔案> 來還原檔案
 //* 設定此檔案不被 merge
 //* master, develope, release
-export const BRANCH = 'ttt123';
+export const BRANCH = 'master';
