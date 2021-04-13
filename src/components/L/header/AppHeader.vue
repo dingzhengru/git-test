bdbf91d7c4932a171b9822f1770946d12203e22b
dingzhengru <qws7869vdx@gmail.com>
2021-04-13 11:52:41 +0800

    feat(header): 完成 lang

--- src/components/L/header/AppHeader.vue
+++ src/components/L/header/AppHeader.vue
@@ -1,14 +1,18 @@
 <template>
   <header class="header">
     <!-- <img :src="siteLogoUrl" class="header__logo" /> -->
-    <div>Logo</div>
-    <button></button>
+    <div class="header__logo"></div>
+    <button class="header__btn header__btn--login">登入\註冊</button>
+    <button class="header__btn header__btn--wallet">
+      <span class="header__btn--wallet__icon">$</span>
+      1,000.00
+    </button>
 
     <div class="header__right">
-      <button class="header__lang-btn">
-        
-      </button>
+      <button class="header__lang-btn" @click="isShowModalLangMenu = true"></button>
     </div>
+
+    <component :is="ModalLangMenu" :isShow="isShowModalLangMenu" @close="isShowModalLangMenu = false" />
   </header>
 </template>
 
@@ -23,8 +27,16 @@ export default {
       default: () => '',
     },
   },
+  data() {
+    return {
+      isShowModalLangMenu: false,
+    };
+  },
   computed: {
     ...mapGetters(['pageTitle', 'siteSetting', 'siteLogoUrl', 'siteIsActive', 'userIsLoggedIn']),
+    ModalLangMenu() {
+      return () => import(`@/${this.siteSetting.components.header.ModalLangMenu}`);
+    },
   },
   methods: {
     changeLang(lang) {
