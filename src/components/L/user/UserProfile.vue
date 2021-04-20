0ac7b58cf0c6c031e96075d17ae7a468d1861d5e
dingzhengru <qws7869vdx@gmail.com>
2021-04-20 15:28:51 +0800

    feat(user-profile): 完成大部分，還差改密碼的 Modal

---
+++ src/components/L/user/UserProfile.vue
@@ -0,0 +1,431 @@
+<template>
+  <ValidationObserver class="user-profile" tag="div" v-slot="{ invalid, handleSubmit, reset }">
+    <form class="user-profile__form" @submit.prevent="handleSubmit(submitUserProfile)" @reset.prevent="reset">
+      <div class="ui-panel-tab">
+        <div class="ui-panel-tab__tabs">
+          <div class="ui-panel-tab__tabs__item">
+            asd
+          </div>
+        </div>
+
+        <div class="ui-panel-tab__content user-profile__content">
+          <div class="user-profile__title user-profile__title--basic">
+            <div class="user-profile__title__icon user-profile__title__icon--basic"></div>
+            <div class="user-profile__title__text user-profile__title__text--basic">
+              {{ $t('user.profile.step.basic') }}
+            </div>
+          </div>
+          <div class="user-profile__basic">
+            <div class="user-profile__field">
+              <label>{{ $t('user.profile.field.registerTime') }}</label>
+              <input type="text" :value="userCreatedDatetime" />
+            </div>
+
+            <ValidationProvider
+              class="user-profile__field"
+              tag="div"
+              :class="[fieldAccount.class]"
+              :name="fieldAccount.name"
+              :rules="fieldAccount.rules"
+              v-show="fieldAccount.isShow"
+              v-slot="{ errors }"
+            >
+              <label>{{ $t(`register.${fieldAccount.name}.placeholder`) }}</label>
+              <input
+                type="text"
+                v-model="fieldAccount.value"
+                @change="changeField(fieldAccount, invalid)"
+                v-if="fieldAccount.isModifiable"
+              />
+              <input type="text" :value="fieldAccount.value" readonly v-else />
+              <div class="user-profile__field__error" v-if="errors.length > 0 && errors[0]">
+                {{ errors[0] }}
+              </div>
+            </ValidationProvider>
+
+            <ValidationProvider
+              class="user-profile__field"
+              tag="div"
+              :class="[fieldRealName.class]"
+              :name="fieldRealName.name"
+              :rules="fieldRealName.rules"
+              v-show="fieldRealName.isShow"
+              v-slot="{ errors }"
+            >
+              <label>{{ $t(`register.${fieldRealName.name}.placeholder`) }}</label>
+              <input
+                type="text"
+                v-model="fieldRealName.value"
+                @change="changeField(fieldRealName, invalid)"
+                v-if="fieldRealName.isModifiable"
+              />
+              <input type="text" :value="fieldRealName.value" readonly v-else />
+              <div class="user-profile__field__error" v-if="errors.length > 0 && errors[0]">
+                {{ errors[0] }}
+              </div>
+            </ValidationProvider>
+
+            <ValidationProvider
+              class="user-profile__field"
+              tag="div"
+              :class="[fieldAccount.class]"
+              :name="fieldMobile.name"
+              :rules="fieldMobile.rules"
+              v-show="fieldMobile.isShow"
+              v-slot="{ errors }"
+            >
+              <label>{{ $t(`register.${fieldMobile.name}.placeholder`) }}</label>
+              <input
+                type="text"
+                v-model="fieldMobile.value"
+                @change="changeField(fieldMobile, invalid)"
+                v-if="fieldMobile.isModifiable"
+              />
+              <input type="text" :value="fieldMobile.value" readonly v-else />
+              <div class="user-profile__field__error" v-if="errors.length > 0 && errors[0]">
+                {{ errors[0] }}
+              </div>
+            </ValidationProvider>
+
+            <ValidationProvider
+              class="user-profile__field"
+              tag="div"
+              :class="[fieldEmail.class]"
+              :name="fieldEmail.name"
+              :rules="fieldEmail.rules"
+              v-show="fieldEmail.isShow"
+              v-slot="{ errors }"
+            >
+              <label>{{ $t(`register.${fieldEmail.name}.placeholder`) }}</label>
+              <input
+                type="text"
+                v-model="fieldEmail.value"
+                @change="changeField(fieldEmail, invalid)"
+                v-if="fieldEmail.isModifiable"
+              />
+              <input type="text" :value="fieldEmail.value" readonly v-else />
+              <div class="user-profile__field__error" v-if="errors.length > 0 && errors[0]">
+                {{ errors[0] }}
+              </div>
+            </ValidationProvider>
+
+            <ValidationProvider
+              class="user-profile__field"
+              tag="div"
+              :class="[fieldNickname.class]"
+              :name="fieldNickname.name"
+              :rules="fieldNickname.rules"
+              v-show="fieldNickname.isShow"
+              v-slot="{ errors }"
+            >
+              <label>{{ $t(`register.${fieldNickname.name}.placeholder`) }}</label>
+              <input type="text" v-model="fieldNickname.value" v-if="fieldNickname.isModifiable" />
+              <input type="text" :value="fieldNickname.value" readonly v-else />
+              <div class="user-profile__field__error" v-if="errors.length > 0 && errors[0]">
+                {{ errors[0] }}
+              </div>
+            </ValidationProvider>
+
+            <ValidationProvider
+              class="user-profile__field"
+              tag="div"
+              :class="[fieldLine.class]"
+              :name="fieldLine.name"
+              :rules="fieldLine.rules"
+              v-show="fieldLine.isShow"
+              v-slot="{ errors }"
+            >
+              <label>{{ $t(`register.${fieldLine.name}.placeholder`) }}</label>
+              <input
+                type="text"
+                v-model="fieldLine.value"
+                @change="changeField(fieldLine, invalid)"
+                v-if="fieldLine.isModifiable"
+              />
+              <input type="text" :value="fieldLine.value" readonly v-else />
+              <div class="user-profile__field__error" v-if="errors.length > 0 && errors[0]">
+                {{ errors[0] }}
+              </div>
+            </ValidationProvider>
+
+            <ValidationProvider
+              class="user-profile__field"
+              tag="div"
+              :class="[fieldBirthday.class]"
+              :name="fieldBirthday.name"
+              :rules="fieldBirthday.rules"
+              v-show="fieldBirthday.isShow"
+              v-slot="{ errors }"
+            >
+              <label>{{ $t(`register.${fieldBirthday.name}.placeholder`) }}</label>
+              <input
+                type="date"
+                v-model="fieldBirthday.value"
+                :min="fieldBirthday.min"
+                :max="fieldBirthday.max"
+                v-if="fieldBirthday.isModifiable"
+              />
+              <input type="date" :value="fieldBirthday.value" readonly v-else />
+              <div class="user-profile__field__error" v-if="errors.length > 0 && errors[0]">
+                {{ errors[0] }}
+              </div>
+            </ValidationProvider>
+
+            <ValidationProvider
+              class="user-profile__field"
+              tag="div"
+              :class="[fieldSkype.class]"
+              :name="fieldSkype.name"
+              :rules="fieldSkype.rules"
+              v-show="fieldSkype.isShow"
+              v-slot="{ errors }"
+            >
+              <label>{{ $t(`register.${fieldSkype.name}.placeholder`) }}</label>
+              <input
+                type="text"
+                v-model="fieldSkype.value"
+                @change="changeField(fieldSkype, invalid)"
+                v-if="fieldSkype.isModifiable"
+              />
+              <input type="text" :value="fieldSkype.value" readonly v-else />
+              <div class="user-profile__field__error" v-if="errors.length > 0 && errors[0]">
+                {{ errors[0] }}
+              </div>
+            </ValidationProvider>
+
+            <ValidationProvider
+              class="user-profile__field"
+              tag="div"
+              :class="[fieldQQ.class]"
+              :name="fieldQQ.name"
+              :rules="fieldQQ.rules"
+              v-show="fieldQQ.isShow"
+              v-slot="{ errors }"
+            >
+              <label>{{ $t(`register.${fieldQQ.name}.placeholder`) }}</label>
+              <input
+                type="text"
+                v-model="fieldQQ.value"
+                @change="changeField(fieldQQ, invalid)"
+                v-if="fieldQQ.isModifiable"
+              />
+              <input type="text" :value="fieldQQ.value" readonly v-else />
+              <div class="user-profile__field__error" v-if="errors.length > 0 && errors[0]">
+                {{ errors[0] }}
+              </div>
+            </ValidationProvider>
+          </div>
+
+          <div class="user-profile__title user-profile__title--bank">
+            <div class="user-profile__title__icon user-profile__title__icon--bank"></div>
+            <div class="user-profile__title__text user-profile__title__text--bank">
+              {{ $t('user.profile.step.bank') }}
+            </div>
+          </div>
+
+          <div class="user-profile__bank">
+            <ValidationProvider
+              class="user-profile__field"
+              tag="div"
+              :class="[fieldBankId1.class]"
+              :name="fieldBankId1.name"
+              :rules="fieldBankId1.rules"
+              v-show="fieldBankId1.isShow"
+              v-slot="{ errors }"
+            >
+              <label>{{ $t(`register.${fieldBankId1.name}.placeholder`) }}</label>
+              <select class="ui-field__select" v-model="fieldBankId1.value" v-if="fieldBankId1.isModifiable">
+                <option :value="item.Value" v-for="item in bankList" :key="item.Value">{{ item.Text }}</option>
+              </select>
+              <input type="text" :value="userBankById(fieldBankId1.value).Lst_BankName" readonly v-else />
+              <div class="user-profile__field__error" v-if="errors.length > 0 && errors[0]">
+                {{ errors[0] }}
+              </div>
+            </ValidationProvider>
+
+            <ValidationProvider
+              class="user-profile__field"
+              tag="div"
+              :class="[fieldBankAccount1.class]"
+              :name="fieldBankAccount1.name"
+              :rules="fieldBankAccount1.rules"
+              v-show="fieldBankAccount1.isShow"
+              v-slot="{ errors }"
+            >
+              <label>{{ $t(`register.${fieldBankAccount1.name}.placeholder`) }}</label>
+              <input
+                type="text"
+                v-model="fieldBankAccount1.value"
+                @change="changeField(fieldBankAccount1, invalid)"
+                v-if="fieldBankAccount1.isModifiable"
+              />
+              <input type="text" :value="fieldBankAccount1.value" readonly v-else />
+              <div class="user-profile__field__error" v-if="errors.length > 0 && errors[0]">
+                {{ errors[0] }}
+              </div>
+            </ValidationProvider>
+
+            <ValidationProvider
+              class="user-profile__field"
+              tag="div"
+              :class="[fieldBankBranchName1.class]"
+              :name="fieldBankBranchName1.name"
+              :rules="fieldBankBranchName1.rules"
+              v-show="fieldBankBranchName1.isShow"
+              v-slot="{ errors }"
+            >
+              <label>{{ $t(`register.${fieldBankBranchName1.name}.placeholder`) }}</label>
+              <input type="text" v-model="fieldBankBranchName1.value" v-if="fieldBankBranchName1.isModifiable" />
+              <input type="text" :value="fieldBankBranchName1.value" readonly v-else />
+              <div class="user-profile__field__error" v-if="errors.length > 0 && errors[0]">
+                {{ errors[0] }}
+              </div>
+            </ValidationProvider>
+
+            <ValidationProvider
+              class="user-profile__field"
+              tag="div"
+              :class="[fieldBankId2.class]"
+              :name="fieldBankId2.name"
+              :rules="fieldBankId2.rules"
+              v-show="fieldBankId2.isShow"
+              v-slot="{ errors }"
+            >
+              <label>{{ $t(`register.${fieldBankId2.name}.placeholder`) }}</label>
+              <select class="ui-field__select" v-model="fieldBankId2.value" v-if="fieldBankId2.isModifiable">
+                <option :value="item.Value" v-for="item in bankList" :key="item.Value">{{ item.Text }}</option>
+              </select>
+              <input type="text" :value="userBankById(fieldBankId2.value).Lst_BankName" readonly v-else />
+              <div class="user-profile__field__error" v-if="errors.length > 0 && errors[0]">
+                {{ errors[0] }}
+              </div>
+            </ValidationProvider>
+
+            <ValidationProvider
+              class="user-profile__field"
+              tag="div"
+              :class="[fieldBankAccount2.class]"
+              :name="fieldBankAccount2.name"
+              :rules="fieldBankAccount2.rules"
+              v-show="fieldBankAccount2.isShow"
+              v-slot="{ errors }"
+            >
+              <label>{{ $t(`register.${fieldBankAccount2.name}.placeholder`) }}</label>
+              <input
+                type="text"
+                v-model="fieldBankAccount2.value"
+                @change="changeField(fieldBankAccount2, invalid)"
+                v-if="fieldBankAccount2.isModifiable"
+              />
+              <input type="text" :value="fieldBankAccount2.value" readonly v-else />
+              <div class="user-profile__field__error" v-if="errors.length > 0 && errors[0]">
+                {{ errors[0] }}
+              </div>
+            </ValidationProvider>
+
+            <ValidationProvider
+              class="user-profile__field"
+              tag="div"
+              :class="[fieldBankBranchName2.class]"
+              :name="fieldBankBranchName2.name"
+              :rules="fieldBankBranchName2.rules"
+              v-show="fieldBankBranchName2.isShow"
+              v-slot="{ errors }"
+            >
+              <label>{{ $t(`register.${fieldBankBranchName2.name}.placeholder`) }}</label>
+              <input type="text" v-model="fieldBankBranchName2.value" v-if="fieldBankBranchName2.isModifiable" />
+              <input type="text" :value="fieldBankBranchName2.value" readonly v-else />
+              <div class="user-profile__field__error" v-if="errors.length > 0 && errors[0]">
+                {{ errors[0] }}
+              </div>
+            </ValidationProvider>
+
+            <ValidationProvider
+              class="user-profile__field"
+              tag="div"
+              :class="[fieldBankId3.class]"
+              :name="fieldBankId3.name"
+              :rules="fieldBankId3.rules"
+              v-show="fieldBankId3.isShow"
+              v-slot="{ errors }"
+            >
+              <label>{{ $t(`register.${fieldBankId3.name}.placeholder`) }}</label>
+              <select class="ui-field__select" v-model="fieldBankId3.value" v-if="fieldBankId3.isModifiable">
+                <option :value="item.Value" v-for="item in bankList" :key="item.Value">{{ item.Text }}</option>
+              </select>
+              <input type="text" :value="userBankById(fieldBankId3.value).Lst_BankName" readonly v-else />
+              <div class="user-profile__field__error" v-if="errors.length > 0 && errors[0]">
+                {{ errors[0] }}
+              </div>
+            </ValidationProvider>
+
+            <ValidationProvider
+              class="user-profile__field"
+              tag="div"
+              :class="[fieldBankAccount3.class]"
+              :name="fieldBankAccount3.name"
+              :rules="fieldBankAccount3.rules"
+              v-show="fieldBankAccount3.isShow"
+              v-slot="{ errors }"
+            >
+              <label>{{ $t(`register.${fieldBankAccount3.name}.placeholder`) }}</label>
+              <input
+                type="text"
+                v-model="fieldBankAccount3.value"
+                @change="changeField(fieldBankAccount3, invalid)"
+                v-if="fieldBankAccount3.isModifiable"
+              />
+              <input type="text" :value="fieldBankAccount3.value" readonly v-else />
+              <div class="user-profile__field__error" v-if="errors.length > 0 && errors[0]">
+                {{ errors[0] }}
+              </div>
+            </ValidationProvider>
+
+            <ValidationProvider
+              class="user-profile__field"
+              tag="div"
+              :class="[fieldBankBranchName3.class]"
+              :name="fieldBankBranchName3.name"
+              :rules="fieldBankBranchName3.rules"
+              v-show="fieldBankBranchName3.isShow"
+              v-slot="{ errors }"
+            >
+              <label>{{ $t(`register.${fieldBankBranchName3.name}.placeholder`) }}</label>
+              <input type="text" v-model="fieldBankBranchName3.value" v-if="fieldBankBranchName3.isModifiable" />
+              <input type="text" :value="fieldBankBranchName3.value" readonly v-else />
+              <div class="user-profile__field__error" v-if="errors.length > 0 && errors[0]">
+                {{ errors[0] }}
+              </div>
+            </ValidationProvider>
+          </div>
+
+          <div class="user-profile__btn">
+            <button class="user-profile__btn--submit ui-btn ui-btn--block" type="submit" :disabled="invalid">
+              {{ $t('ui.button.submit') }}
+            </button>
+            <button class="user-profile__btn--reset ui-btn ui-btn--block" type="reset" @click="resetForm">
+              {{ $t('ui.button.reset') }}
+            </button>
+          </div>
+        </div>
+      </div>
+    </form>
+  </ValidationObserver>
+</template>
+
+<script>
+import mixinUserProfile from '@/mixins/userProfile';
+import { mapGetters } from 'vuex';
+import { ValidationObserver, ValidationProvider } from 'vee-validate';
+export default {
+  name: 'UserProfile',
+  mixins: [mixinUserProfile],
+  components: {
+    ValidationObserver,
+    ValidationProvider,
+  },
+  computed: {
+    ...mapGetters(['siteSetting', 'siteFullCss']),
+  },
+};
+</script>
--- src/mixins/userProfile.js
+++ src/mixins/userProfile.js
@@ -18,6 +18,25 @@ export default {
       fieldList: registerFieldList,
       bankList: [],
       fieldListOld: [],
+
+      fieldAccount: {},
+      fieldNickname: {},
+      fieldRealName: {},
+      fieldMobile: {},
+      fieldEmail: {},
+      fieldBirthday: {},
+      fieldSkype: {},
+      fieldQQ: {},
+      fieldLine: {},
+      fieldBankId1: {},
+      fieldBankAccount1: {},
+      fieldBankBranchName1: {},
+      fieldBankId2: {},
+      fieldBankAccount2: {},
+      fieldBankBranchName2: {},
+      fieldBankId3: {},
+      fieldBankAccount3: {},
+      fieldBankBranchName3: {},
     };
   },
   methods: {
@@ -39,6 +58,7 @@ export default {
         }
       }
       this.fieldListOld = this.$deepClone(this.fieldList);
+      this.setFields();
     },
     async submitUserProfile() {
       const requestData = {};
@@ -85,9 +105,74 @@ export default {
     resetForm() {
       this.fieldList = this.$deepClone(this.fieldListOld);
     },
+    setFields() {
+      console.log('setFields');
+      this.fieldList.forEach(item => {
+        switch (item.name) {
+          case 'Add_Account': {
+            return (this.fieldAccount = item);
+          }
+          case 'Add_NickName': {
+            return (this.fieldNickname = item);
+          }
+          case 'Add_RealName': {
+            return (this.fieldRealName = item);
+          }
+          case 'Add_Mobile': {
+            return (this.fieldMobile = item);
+          }
+          case 'Add_Email': {
+            return (this.fieldEmail = item);
+          }
+          case 'Add_Birthday': {
+            return (this.fieldBirthday = item);
+          }
+          case 'Add_Skype': {
+            return (this.fieldSkype = item);
+          }
+          case 'Add_QQ': {
+            return (this.fieldQQ = item);
+          }
+          case 'Add_Line': {
+            return (this.fieldLine = item);
+          }
+          case 'Add_BankId1': {
+            return (this.fieldBankId1 = item);
+          }
+          case 'Add_BankAccount1': {
+            return (this.fieldBankAccount1 = item);
+          }
+          case 'Add_BankBranchName1': {
+            return (this.fieldBankBranchName1 = item);
+          }
+          case 'Add_BankId2': {
+            return (this.fieldBankId2 = item);
+          }
+          case 'Add_BankAccount2': {
+            return (this.fieldBankAccount2 = item);
+          }
+          case 'Add_BankBranchName2': {
+            return (this.fieldBankBranchName2 = item);
+          }
+          case 'Add_BankId3': {
+            return (this.fieldBankId3 = item);
+          }
+          case 'Add_BankAccount3': {
+            return (this.fieldBankAccount3 = item);
+          }
+          case 'Add_BankBranchName3': {
+            return (this.fieldBankBranchName3 = item);
+          }
+        }
+      });
+    },
   },
   mounted() {
     this.getRegisterAdvanceNew();
   },
-  watch: {},
+  watch: {
+    fieldList() {
+      this.setFields();
+    },
+  },
 };
