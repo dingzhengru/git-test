84074f9850b0c25d285ede209f5a6e7b0eb634ff
dingzhengru <qws7869vdx@gmail.com>
2021-05-06 15:06:27 +0800

    feat(modal-lang): 完成

--- src/components/PA/header/ModalLangMenu.vue
+++ src/components/PA/header/ModalLangMenu.vue
@@ -1,11 +1,12 @@
 <template>
   <AppModal @close="$emit('close')">
-    <div class="modal-lang-menu">
+    <div class="ui-modal modal-lang">
       <div class="ui-box-close" @click="$emit('close')"></div>
 
-      <div class="modal-lang-menu__btn-block">
-        <button
-          class="modal-lang-menu__btn"
+      <div class="ui-modal__title">{{ $t('ui.label.langSelect') }}</div>
+      <div class="ui-modal__content modal-lang__content">
+        <div
+          class="modal-lang__item"
           v-for="item in langList"
           :key="item.Lst_Locales_Code"
           @click="
@@ -13,8 +14,9 @@
             $emit('close');
           "
         >
-          {{ item.Lst_Locales_Name }}
-        </button>
+          <button class="modal-lang__item__btn">{{ item.Lst_Locales_Name }}</button>
+          <span class="modal-lang__item__selected" v-show="item.Lst_Locales_Code === lang"></span>
+        </div>
       </div>
     </div>
   </AppModal>
