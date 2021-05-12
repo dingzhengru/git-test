dcb16f565b3742d8b461852529f24b885e914e25
dingzhengru <qws7869vdx@gmail.com>
2021-05-12 16:26:10 +0800

    feat(transaction-wallet): 新增維護館別不可轉帳的功能

--- src/components/PA/transaction/TransactionWallet.vue
+++ src/components/PA/transaction/TransactionWallet.vue
@@ -19,10 +19,13 @@
         class="transaction-wallet__product__item"
         v-for="item in userGamePointListNoWallet"
         :key="item.Product_id"
-        @click="$store.dispatch('openModalTransfer', item)"
+        @click="openTransferModal(item)"
       >
         <div class="transaction-wallet__product__item__name">{{ item.Product_Name }}</div>
         <div class="transaction-wallet__product__item__amount">{{ $numeral(item.Point).format('0,0.00') }}</div>
+        <div class="transaction-wallet__product__item__overlay" v-show="!productEnabled(item)">
+          {{ $t('ui.label.underMaintenance') }}
+        </div>
       </div>
     </div>
   </div>
@@ -37,9 +40,20 @@ export default {
   name: 'TransactionWallet',
   mixins: [mixinStyleLoader, mixinAccountWallet],
   computed: {
-    ...mapGetters(['userGamePointList', 'userGamePointWallet']),
+    ...mapGetters(['userGamePointList', 'userGamePointWallet', 'productById']),
     userGamePointListNoWallet() {
       return this.userGamePointList.filter(item => item.Product_id !== 9999);
+    },
+  },
+  methods: {
+    productEnabled(product) {
+      return this.productById(product.Product_id).Lst_Site_Product_Status === 0;
+    },
+    openTransferModal(product) {
+      if (this.productEnabled(product) === false) {
+        return;
+      }
+      this.$store.dispatch('openModalTransfer', product);
     },
   },
   mounted() {
