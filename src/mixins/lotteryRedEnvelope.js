765ededad6950392f10c180de65139ecb3793a45
dingzhengru <qws7869vdx@gmail.com>
2021-05-18 09:07:26 +0800

    feat(win-wheel): 轉盤，途中

--- src/mixins/lotteryRedEnvelope.js
+++ src/mixins/lotteryRedEnvelope.js
@@ -1,15 +1,6 @@
 import { mapGetters } from 'vuex';
 import { apiPlayLottery, apiPlayLotteryResult } from '@/api/lottery';
 
-//* Loading 圖片
-import lotteryLoadingImage from '@/assets/common/lottery/loading.svg';
-
-//* 紅包遊戲的圖片
-import cardFrontImgage from '@/assets/common/lottery/redEnvelope/lucky--close1.png';
-import cardBackImgage from '@/assets/common/lottery/redEnvelope/lucky--open3.png';
-import dialogImgage from '@/assets/common/lottery/redEnvelope/bg-winPrize.png';
-import envelopeLoadingImgage from '@/assets/common/lottery/redEnvelope/eclipse.svg';
-
 export default {
   name: 'MixinLotteryRedEnvelope',
   computed: {
@@ -27,16 +18,6 @@ export default {
 
       // 錯誤訊息
       redEnvelopeErrorMessage: '',
-
-      //* 紅包
-      redEnvelopeStyle: {
-        loadingImgUrl: lotteryLoadingImage, //遊戲loading
-        activityImgUrl: '', //(獎項列表API)活動標題圖片
-        cardFrontImgUrl: cardFrontImgage, //翻牌前
-        cardBackImgUrl: cardBackImgage, //翻牌後
-        dialogImgUrl: dialogImgage, //彈跳視窗背景
-        envelopeLoadingImgUrl: envelopeLoadingImgage, //紅包loading
-      },
     };
   },
   methods: {
--- src/mixins/lotteryWinWheel.js
+++ src/mixins/lotteryWinWheel.js
@@ -1,16 +1,6 @@
 import { mapGetters } from 'vuex';
 import { apiPlayLottery, apiPlayLotteryResult } from '@/api/lottery';
 
-//* Loading 圖片
-import lotteryLoadingImage from '@/assets/common/lottery/loading.svg';
-
-//* 轉盤遊戲的圖片
-// import wheelContainerBackgroundImage from '@/assets/common/lottery/winWheel/container-bg.png';
-import wheelPrizeImage from '@/assets/common/lottery/winWheel/prize.png';
-import wheelPointerImage from '@/assets/common/lottery/winWheel/pointercn.png';
-import wheelLodaingImage from '@/assets/common/lottery/winWheel/wheel-loading.svg';
-import wheelBackgroundImage from '@/assets/common/lottery/winWheel/wheel-bg.png';
-
 export default {
   name: 'MixinLotteryWinWheel',
   computed: {
@@ -29,35 +19,6 @@ export default {
 
       // 錯誤訊息
       wheelErrorMessage: '',
-
-      //* 轉盤樣式
-      wheelStyle: {
-        width: 580, //轉盤寬度
-        height: 580, //轉盤高度
-        loadingImgUrl: lotteryLoadingImage, //遊戲loading
-        activityImgUrl: '', //(獎項表單API)活動標題圖片
-        prizeImgUrl: wheelPrizeImage, //禮物
-        pointerImgUrl: wheelPointerImage, //開始抽獎按鈕
-        wheelLodaingImgUrl: wheelLodaingImage, //轉盤loading
-        wheelBackgroundImgUrl: wheelBackgroundImage, //轉盤背景
-        wheelColor1: '#FFFFFF', //轉盤顏色1
-        wheelColor2: '#FFF4D6', //轉盤顏色2
-      },
-      //* 轉盤設定
-      wheelOptions: {
-        textFontSize: 18,
-        outerRadius: 255, //外圓半徑
-        innerRadius: 0, //內圓半徑(預設0)
-        lineWidth: 0,
-        drawMode: 'segmentImage', // code: 以文字及style顯示, image: 背景圖片顯示, segmentImage: 每個欄位一張圖片
-        segmentImageBackground: true, // 欄位圖片是否繪製背景色(fillStyle)
-        // wheelImage: "/planes.png", // (選)背景圖片位置
-        animation: {
-          type: 'spinToStop',
-          duration: 2,
-          spins: 5,
-        },
-      },
     };
   },
   methods: {
