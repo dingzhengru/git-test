549094263a4b929336eef50902c1bedf23679bd4
dingzhengru <qws7869vdx@gmail.com>
2021-06-15 17:46:22 +0800

    feat(main): update log

--- src/main.js
+++ src/main.js
@@ -20,7 +20,7 @@ import {
   cookieGetIsPreview,
 } from '@/utils/cookie';
 
-console.log('log: update test');
+console.log('log: update vue.config: sideEffects');
 console.log(process.env.NODE_ENV);
 
 //* CSS
