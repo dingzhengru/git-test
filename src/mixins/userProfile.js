31bf2cc27fce533ed8ef5d45d88f925cd7ba85d7
dingzhengru <qws7869vdx@gmail.com>
2021-06-21 17:42:58 +0800

    feat(profile): 取消套用 Visibled

--- src/mixins/userProfile.js
+++ src/mixins/userProfile.js
@@ -21,7 +21,8 @@ export default {
       return this.fieldList.filter(item => item.name.includes('Bank') === false);
     },
     bankListEnabled() {
-      return this.bankList.filter(item => item.Visibled);
+      return this.bankList;
+      // return this.bankList.filter(item => item.Visibled);
     },
   },
   data() {
