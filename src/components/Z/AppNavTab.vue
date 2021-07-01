fc775c6990d61093beac526001044863e7867fd8
dingzhengru <qws7869vdx@gmail.com>
2021-07-01 16:33:25 +0800

    feat(nav): 修改 nav 參數 (ex: name => route)

--- src/components/Z/AppNavTab.vue
+++ src/components/Z/AppNavTab.vue
@@ -4,16 +4,16 @@
     <div
       class="nav-tab__item"
       :class="[
-        item.class,
+        item.class ? item.class : '',
         {
-          active: item.link == $route.name || (item.otherActiveRoute && item.otherActiveRoute.includes($route.name)),
+          active: item.route == $route.name || (item.otherActiveRoute && item.otherActiveRoute.includes($route.name)),
         },
       ]"
       :id="item.id ? $getObjectValueByDotString($idMapper, item.id) : ''"
       v-for="item in list"
-      :key="item.name"
+      :key="item.route"
     >
-      <router-link class="nav-tab__item__link" :to="{ name: item.link }">
+      <router-link class="nav-tab__item__link" :to="{ name: item.route }">
         {{ $t(item.text) }}
       </router-link>
     </div>
