535b3b9e2e41b1cc568f6f774b164a7660cc39ee
dingzhengru <qws7869vdx@gmail.com>
2021-07-02 12:01:02 +0800

    feat(deposit): 修正切換語系，nav 未先重置

--- src/components/PA/transaction/deposit/TransactionDepositHome.vue
+++ src/components/PA/transaction/deposit/TransactionDepositHome.vue
@@ -6,7 +6,7 @@
         :class="{ active: navIsActive(item) }"
         v-for="(item, index) in navList"
         :key="index"
-        @click="$router.push({ name: item.route, params: item.params })"
+        @click="$router.push({ name: item.route, params: item.params }).catch(() => {})"
       >
         {{ $te(item.text) ? $t(item.text) : item.text }}
       </div>
@@ -37,65 +37,8 @@ export default {
   computed: {
     ...mapGetters(['siteSetting']),
   },
-  data() {
-    return {
-      navList: [
-        {
-          route: 'TransactionDepositBase',
-          text: 'transaction.deposit.nav.base',
-          params: {},
-        },
-        // {
-        //   route: 'TransactionDepositAutoCash',
-        //   text: 'transaction.deposit.nav.autoCash',
-        //   params: {},
-        // },
-      ],
-    };
-  },
-  methods: {
-    navIsActive(nav) {
-      if (this.$isObjEmpty(nav.params)) {
-        return this.$route.name === nav.route;
-      }
-
-      const payment = nav.params.payment;
-      return this.$route.name === nav.route && this.$route.params.payment === payment;
-    },
-  },
   mounted() {
     this.importStyleByFilename('transaction-deposit');
-  },
-  watch: {
-    depositInfo() {
-      // let index = 1;
-      // this.depositInfo.paymentSelect.forEach(item => {
-      //   this.navList.splice(index, 0, {
-      //     route: 'TransactionDepositThirdPartyPayment',
-      //     params: { payment: item.Value },
-      //     text: item.Text,
-      //   });
-      //   index = index + 1;
-      // });
-
-      this.depositInfo.paymentSelect.forEach(item => {
-        this.navList.push({
-          route: 'TransactionDepositThirdPartyPayment',
-          params: { payment: item.Value },
-          text: item.Text,
-        });
-      });
-
-      if (this.$env === 'development') {
-        if (this.depositInfo.AutoCashCount > 0) {
-          this.navList.push({
-            route: 'TransactionDepositAutoCash',
-            text: 'transaction.deposit.nav.autoCash',
-            params: {},
-          });
-        }
-      }
-    },
   },
 };
 </script>
