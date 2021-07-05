7bf3092db0317c91ae15ec233b79ec3c3c0cb6d6
dingzhengru <qws7869vdx@gmail.com>
2021-07-05 17:14:49 +0800

    feat(user-mgm): 完成橫版 nav

--- src/components/PA/user/UserPromotionMGM.vue
+++ src/components/PA/user/UserPromotionMGM.vue
@@ -1,10 +1,34 @@
 <template>
   <div class="user-promotion-gmg">
-    <router-view />
+    <div class="ui-panel-tab">
+      <component :is="PanelTabs" :list="tabList" />
+
+      <div class="ui-panel-tab__content user-promotion-gmg__content">
+        <div class="user-promotion-gmg__nav">
+          <div
+            class="user-promotion-gmg__nav__item"
+            :class="{ active: $route.name === 'UserPromotionMGMDetail' }"
+            @click="$router.push({ name: 'UserPromotionMGMDetail' }).catch(() => {})"
+          >
+            {{ $t('user.mgm.nav.detail') }}
+          </div>
+          <div
+            class="user-promotion-gmg__nav__item"
+            :class="{ active: $route.name === 'UserPromotionMGMBonus' }"
+            @click="$router.push({ name: 'UserPromotionMGMBonus' }).catch(() => {})"
+          >
+            {{ $t('user.mgm.nav.bonus') }}
+          </div>
+        </div>
+
+        <router-view />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
 import mixinUserPromotionMGM from '@/mixins/userPromotionMGM';
 // import VueQr from 'vue-qr';
 export default {
@@ -13,6 +37,28 @@ export default {
   // components: {
   //   VueQr,
   // },
+  computed: {
+    ...mapGetters(['lang', 'siteSetting']),
+    PanelTabs() {
+      return () => import(`@/${this.siteSetting.components.user.PanelTabs}`);
+    },
+  },
+  data() {
+    return {
+      tabList: [
+        {
+          route: 'UserProfile',
+          text: 'header.menu.profile',
+          otherActiveRoute: [],
+        },
+        {
+          route: 'UserPromotionMGMDetail',
+          text: 'user.nav.promotionMGM',
+          otherActiveRoute: ['UserPromotionMGMBonus'],
+        },
+      ],
+    };
+  },
 };
 </script>
 
